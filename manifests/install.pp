35b115f9793d91935f6364df111c5107424f729e
root <root@jeffkinjerski1c.mylabserver.com>
2019-07-30 19:43:20 +0000

    Refactored params.pp in favor of Hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,9 @@
 # @summary A short summary of the purpose of this class
 #  Installs the base Apache Package
 #
-class apache::install (
-  $install_Name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure, 
-) inherits apache::params {
-  package { "${install_Name}":
-    ensure  => $install_ensure,
+class apache::install {
+  package { "${apache::install_Name}":
+    ensure  => $apache::install_ensure,
   }
 }
 
